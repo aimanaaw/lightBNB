672ea445bf95f166eb6f9d990b30f8e15dcbb28b
Aiman Azmie <aimanaaw@gmail.com>
2019-10-17 18:15:08 +0000

    Get all a user's reservations when they are logged in

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -59,7 +59,19 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  const queryString = `SELECT properties.id, properties.title, properties.cost_per_night, reservations.start_date, avg(property_reviews.rating)
+  FROM reservations
+  JOIN properties ON reservations.property_id = properties.id
+  JOIN property_reviews ON properties.id = property_reviews.property_id
+  WHERE reservations.guest_id = $1 AND reservations.end_date < now()::date
+  GROUP BY properties.id, reservations.id
+  ORDER BY reservations.start_date
+  LIMIT $2;`
+  const values = [guest_id, limit]
+  return pool.query(queryString, values)
+  .then(res => {
+    return getAllProperties(null, 2);
+  });
 }
 exports.getAllReservations = getAllReservations;
 
